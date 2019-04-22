14d87d8ad9a392d842d4dee3af3d2a412f0433e8
Banco de Comercio <bancodecomercio@sypnopsis02.comercio.local>
2019-04-22 14:20:14 -0500

    Romario Coronel Test iOS

---
+++ TestIOS/TestIOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestIOS
+//
+//  Created by Banco de Comercio on 4/22/19.
+//  Copyright © 2019 fd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let romario: String = "Romario Coronel Alvarez ---> man"
+        let romario1: String = "Romario Coronel Alvarez ---> man"
+        let romario3: String = "Romario Coronel Alvarez ---> man"
+        let romario4: String = "Romario Coronel Alvarez ---> man"
+    let romario5: String = "Romario Coronel Alvarez ---> man"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
+
