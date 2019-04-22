15f8df205d4dd1dedcedd99fd54951fa6feae061
Banco de Comercio <romario.coronel0723@gmail.com>
2019-04-22 16:20:22 -0500

    Romario Coronel Bitckup

---
+++ testBitbucket/testiOSBitbucketromario0322/testiOSBitbucketromario0322/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  testiOSBitbucketromario0322
+//
+//  Created by Banco de Comercio on 4/22/19.
+//  Copyright © 2019 fd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let romario :String = "Romario Coronel Alvarez"
+    let romario1 :String = "Romario Coronel Alvarez"
+    let romario2 :String = "Romario Coronel Alvarez"
+    let romario3 :String = "Romario Coronel Alvarez"
+    let romario4 :String = "Romario Coronel Alvarez"
+    let romario5 :String = "Romario Coronel Alvarez"
+    let romario6 :String = "Romario Coronel Alvarez"
+    let romario7 :String = "Romario Coronel Alvarez"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
